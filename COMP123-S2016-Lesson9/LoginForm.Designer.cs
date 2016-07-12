89f3f425b390298f6709aa61b7a8fbbe111092e7
300872772 <mamun_131@hotmail.com>
2016-07-12 14:19:18 -0300

    validation

--- COMP123-S2016-Lesson9/LoginForm.Designer.cs
+++ COMP123-S2016-Lesson9/LoginForm.Designer.cs
@@ -28,89 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.UserNameLabel = new System.Windows.Forms.Label();
-            this.UsernameTextBox = new System.Windows.Forms.TextBox();
-            this.PasswordLabel = new System.Windows.Forms.Label();
-            this.PasswordTextBox = new System.Windows.Forms.TextBox();
             this.LoginButton = new System.Windows.Forms.Button();
-            this.MessagLabel = new System.Windows.Forms.Label();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.MessageLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // UserNameLabel
+            // LoginButton
             // 
-            this.UserNameLabel.AutoSize = true;
-            this.UserNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UserNameLabel.Location = new System.Drawing.Point(31, 105);
-            this.UserNameLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
-            this.UserNameLabel.Name = "UserNameLabel";
-            this.UserNameLabel.Size = new System.Drawing.Size(105, 24);
-            this.UserNameLabel.TabIndex = 0;
-            this.UserNameLabel.Text = "User Name";
-            this.UserNameLabel.Click += new System.EventHandler(this.label1_Click);
+            this.LoginButton.Location = new System.Drawing.Point(336, 299);
+            this.LoginButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(150, 44);
+            this.LoginButton.TabIndex = 0;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(64, 119);
+            this.UsernameLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(125, 25);
+            this.UsernameLabel.TabIndex = 1;
+            this.UsernameLabel.Text = "User Name ";
             // 
             // UsernameTextBox
             // 
-            this.UsernameTextBox.Location = new System.Drawing.Point(31, 136);
-            this.UsernameTextBox.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.UsernameTextBox.Location = new System.Drawing.Point(64, 156);
+            this.UsernameTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.UsernameTextBox.Name = "UsernameTextBox";
-            this.UsernameTextBox.Size = new System.Drawing.Size(373, 38);
-            this.UsernameTextBox.TabIndex = 0;
+            this.UsernameTextBox.Size = new System.Drawing.Size(422, 31);
+            this.UsernameTextBox.TabIndex = 2;
+            this.UsernameTextBox.Leave += new System.EventHandler(this.UsernameTextBox_Leave);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(64, 240);
+            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '.';
+            this.PasswordTextBox.Size = new System.Drawing.Size(422, 31);
+            this.PasswordTextBox.TabIndex = 4;
+            this.PasswordTextBox.Leave += new System.EventHandler(this.PasswordTextBox_Leave);
             // 
             // PasswordLabel
             // 
             this.PasswordLabel.AutoSize = true;
-            this.PasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PasswordLabel.Location = new System.Drawing.Point(31, 191);
-            this.PasswordLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.PasswordLabel.Location = new System.Drawing.Point(64, 204);
+            this.PasswordLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.PasswordLabel.Name = "PasswordLabel";
-            this.PasswordLabel.Size = new System.Drawing.Size(92, 24);
-            this.PasswordLabel.TabIndex = 2;
-            this.PasswordLabel.Text = "Password";
+            this.PasswordLabel.Size = new System.Drawing.Size(112, 25);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password ";
             // 
-            // PasswordTextBox
+            // MessageLabel
             // 
-            this.PasswordTextBox.Location = new System.Drawing.Point(31, 222);
-            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.PasswordChar = '.';
-            this.PasswordTextBox.Size = new System.Drawing.Size(373, 38);
-            this.PasswordTextBox.TabIndex = 1;
-            // 
-            // LoginButton
-            // 
-            this.LoginButton.Location = new System.Drawing.Point(265, 289);
-            this.LoginButton.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.LoginButton.Name = "LoginButton";
-            this.LoginButton.Size = new System.Drawing.Size(139, 46);
-            this.LoginButton.TabIndex = 2;
-            this.LoginButton.Text = "Login";
-            this.LoginButton.UseVisualStyleBackColor = true;
-            // 
-            // MessagLabel
-            // 
-            this.MessagLabel.AutoSize = true;
-            this.MessagLabel.Location = new System.Drawing.Point(31, 27);
-            this.MessagLabel.Name = "MessagLabel";
-            this.MessagLabel.Size = new System.Drawing.Size(170, 31);
-            this.MessagLabel.TabIndex = 3;
-            this.MessagLabel.Text = "Please Login";
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Location = new System.Drawing.Point(64, 27);
+            this.MessageLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(125, 25);
+            this.MessageLabel.TabIndex = 5;
+            this.MessageLabel.Text = "User Name ";
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 411);
-            this.Controls.Add(this.MessagLabel);
-            this.Controls.Add(this.LoginButton);
+            this.ClientSize = new System.Drawing.Size(568, 502);
+            this.Controls.Add(this.MessageLabel);
             this.Controls.Add(this.PasswordTextBox);
             this.Controls.Add(this.PasswordLabel);
             this.Controls.Add(this.UsernameTextBox);
-            this.Controls.Add(this.UserNameLabel);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.LoginButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "LoginForm";
-            this.Text = "Login";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,12 +119,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label UserNameLabel;
-        private System.Windows.Forms.TextBox UsernameTextBox;
-        private System.Windows.Forms.Label PasswordLabel;
-        private System.Windows.Forms.TextBox PasswordTextBox;
         private System.Windows.Forms.Button LoginButton;
-        private System.Windows.Forms.Label MessagLabel;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Label MessageLabel;
     }
 }
-
--- COMP123-S2016-Lesson9/LoginForm.cs
+++ COMP123-S2016-Lesson9/LoginForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,19 +13,93 @@ namespace COMP123_S2016_Lesson9
 {
     public partial class LoginForm : Form
     {
+        /** 
+          * <summary> 
+          * This is the default constructor for the LoginForm Class 
+          * </summary> 
+          *  
+          * @constructor LoginForm 
+          */
+
         public LoginForm()
         {
             InitializeComponent();
         }
+        // PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++++++++ 
 
-        private void Form1_Load(object sender, EventArgs e)
+        /** 
+     * <summary> 
+     * This method clears the UsernameTextBox and the PasswordTextBox 
+     *           * </summary> 
+     *  
+     * @private 
+     * @method _clearForm 
+     * @returns {void} 
+     */
+        private void _clearForm()
         {
+            UsernameTextBox.Clear();
+            PasswordTextBox.Clear();
+        }
+        /** 
+         * <summary> 
+         * This is the LoginButton Click event handler 
+          * </summary> 
+          *  
+          * @private 
+          * @method LoginButton_Click 
+          * @param {object} sender 
+          * @param {EventArgs} e 
+          * @returns {void} 
+          */
 
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Username: " + UsernameTextBox.Text);
+            Debug.WriteLine("Password: " + PasswordTextBox.Text);
+            this._clearForm();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            LoginButton.Enabled = false;
+        }
+
+        private void UsernameTextBox_Leave(object sender, EventArgs e)
+        {
+            if (UsernameTextBox.Text.Length<6)
+            {
+                UsernameTextBox.Focus();
+                UsernameTextBox.SelectAll();
+                MessageLabel.Text = "User name is too short";
+                MessageLabel.ForeColor = Color.Red;
+            }
+            else
+            {
+                MessageLabel.Text = "Please Login";
+                MessageLabel.ForeColor = Color.Black;
+            }
+        }
+
+        private void PasswordTextBox_Leave(object sender, EventArgs e)
+        {
+            if (PasswordTextBox.Text.Length < 6)
+            {
+                PasswordTextBox.Focus();
+                PasswordTextBox.SelectAll();
+                MessageLabel.Text = "Password is too short";
+                MessageLabel.ForeColor = Color.Red;
+            }
+            else
+            {
+                MessageLabel.Text = "Please Login";
+                MessageLabel.ForeColor = Color.Black;
+            }
+
+                LoginButton.Enabled = true;
+            
         }
     }
 }
